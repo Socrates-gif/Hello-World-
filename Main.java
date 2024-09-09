2db059967abb725a346febb98d4e9b6b67518b9f
Socrates-gif <socratest3@nycstudents.net>
2024-09-09 01:16:06 +0000

    first commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main 
+{
+    public static void main(String[] args)
+    {
+        System.out.print("Hello!");
+        System.out.println();
+        System.out.println("How was your day today?");
+    }
+}
